e63d589a69d6f54c32011f59c9a973eecc4b8692
Jamie Pham <jtkp66@gmail.com>
2018-10-23 18:53:10 -0700

    added email contact form

---
+++ emailForm.js
@@ -0,0 +1,35 @@
+{/* <div class="col-md-6">
+            <form id="contact-form" method="post" action="/contact" class="contact-form">
+              <div class="controls">
+                <div class="row">
+                  <div class="col-sm-6">
+                    <div class="form-group">
+                      <label for="name">Your firstname *</label>
+                      <input type="text" name="firstName" placeholder="Enter your firstname" required="required" class="form-control">
+                    </div>
+                  </div>
+                  <div class="col-sm-6">
+                    <div class="form-group">
+                      <label for="surname">Your lastname *</label>
+                      <input type="text" name="lastName" placeholder="Enter your  lastname" required="required" class="form-control">
+                    </div>
+                  </div>
+                </div>
+                <div class="form-group">
+                  <label for="surname">Your email *</label>
+                  <input type="email" name="email" placeholder="Enter your  email" required="required" class="form-control">
+                </div>
+                <div class="form-group">
+                  <label for="surname">Your message for me *</label>
+                  <textarea rows="4" name="message" placeholder="Enter your message" required="required" class="form-control"></textarea>
+                </div>
+                <div class="text-center">
+                  <input type="submit" name="name" value="Send message" class="btn btn-primary btn-block">
+                </div>
+              </div>
+            </form>
+            <% if (mailSent) { %>
+              <p>Thanks for the mail,
+                <%= firstName%>. I'll get back to you soon.</p>
+              <% } %>
+          </div> */}
